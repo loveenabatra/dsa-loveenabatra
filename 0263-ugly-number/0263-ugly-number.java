9c64ba0d708087fd26eff860ecbdd019e332b760
loveenabatra <143820075+loveenabatra@users.noreply.github.com>
2026-08-09 00:42:18 +0530

    Time: 1 ms (98.99%), Space: 42.8 MB (15.24%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0){
+            return false;
+        }
+        int[] fac={2,3,5};
+        for(int i:fac){
+            while(n%i==0){
+                n/=i;
+            }
+        }
+        return n==1;
+    }
+}
